3dddaed3dbe777cb16c8c278680af50bc303156f
Code Sandbox <hello@codesandbox.io>
2023-02-23 17:35:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.545984554abfa81bdc6ead9ef191e487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c33e89e6f8871e34cd6d27b6dabfcfb",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/index.html"
+  },
+  {
+    "revision": "a163224f16320bb14201",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/js/2.60cc8d96.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/js/2.60cc8d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b2b8c0c9a169933236",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/js/main.484c204b.chunk.js"
+  },
+  {
+    "revision": "522cb7c5e5cb5a19ef5c",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/js/runtime-main.f0a14a28.js"
+  },
+  {
+    "revision": "39ff8275793168260a362146c680785d",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/media/bg1.39ff8275.jpg"
+  },
+  {
+    "revision": "4913401e4892d51d574279a5e77374ae",
+    "url": "https://jasmeenkaurttn.github.io/csb-69scuo/static/media/google-icon.4913401e.png"
+  }
+]);
